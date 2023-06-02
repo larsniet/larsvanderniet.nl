d09c43f5398b998292347f83e145a6425857a950
larsniet <lvdnbusiness@icloud.com>
2023-06-02 20:23:49 +0200

    Added gooletag

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -5,6 +5,9 @@ import { dir } from 'i18next'
 import { getTranslation, languages } from '@/lib/i18n'
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
+
+const gtag = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }))
@@ -28,6 +31,7 @@ export default async function RootLayout({
 
   return (
     <html lang={lang} dir={dir(lang)} className="h-full">
+      {gtag && <GoogleAnalytics NEXT_PUBLIC_GOOGLE_ANALYTICS_ID={gtag} />}
       <body className="h-full bg-white dark:bg-zinc-900">
         <div className="fixed inset-0 flex justify-center sm:px-8">
           <div className="flex w-full max-w-7xl lg:px-8">
